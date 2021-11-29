3579183125f7adf33d11598fd98c14303232ae63
Aleksandr Koynosov <abenefic@gmail.com>
2021-11-29 22:53:29 +0300

    HW4

--- core/src/ru/gb/screen/impl/GameScreen.java
+++ core/src/ru/gb/screen/impl/GameScreen.java
@@ -1,13 +1,21 @@
 package ru.gb.screen.impl;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.gb.math.Rect;
 import ru.gb.screen.BaseScreen;
 import ru.gb.sprite.impl.Background;
+import ru.gb.sprite.impl.Ship;
+import ru.gb.sprite.impl.Star;
 
 public class GameScreen extends BaseScreen {
+
+    private static final int STAR_COUNT = 256;
+    private TextureAtlas atlas;
+    private Star[] stars;
+    private Ship ship;
 
     private Texture bg;
     private Background background;
@@ -17,6 +25,14 @@ public class GameScreen extends BaseScreen {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
+
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
+
+        ship = new Ship(atlas);
     }
 
     @Override
@@ -30,17 +46,23 @@ public class GameScreen extends BaseScreen {
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
+        ship.resize(worldBounds);
     }
 
     @Override
     public void dispose() {
         super.dispose();
         bg.dispose();
+        atlas.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        return super.touchDown(touch, pointer, button);
+        super.touchDown(touch, pointer, button);
+        return ship.touchDown(touch, pointer, button);
     }
 
     @Override
@@ -48,13 +70,40 @@ public class GameScreen extends BaseScreen {
         return super.touchUp(touch, pointer, button);
     }
 
-    private void update(float delta) {
+    @Override
+    public boolean touchDragged(Vector2 touch, int pointer) {
+        super.touchDragged(touch, pointer);
+        return ship.touchDragged(touch, pointer);
+    }
 
+    @Override
+    public boolean keyDown(int keycode) {
+        super.keyDown(keycode);
+        ship.keyDown(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        super.keyUp(keycode);
+        ship.keyUp(keycode);
+        return false;
+    }
+
+    private void update(float delta) {
+        for (Star star : stars) {
+            star.update(delta);
+        }
+        ship.update(delta);
     }
 
     private void draw() {
         batch.begin();
         background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
+        ship.draw(batch);
         batch.end();
     }
 }
